734f305b98498b3092dba0cac287ec60397f2042
ShiRu19 <87590345+ShiRu19@users.noreply.github.com>
2022-03-10 16:34:45 +0800

    Add game4.10. Prepare the bmp picture of the character.

---
+++ game4.10/Source/resource.h
@@ -0,0 +1,83 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 產生的 Include 檔案。
+// 由 game.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_SOCKETS_INIT_FAILED         104
+#define IDR_MAINFRAME                   128
+#define IDR_GAMETYPE                    129
+#define IDB_BALL                        130
+#define IDB_RACKET                      131
+#define IDB_ERASER1                     131
+#define IDB_BITMAP1                     132
+#define IDB_BACKGROUND                  132
+#define IDB_CORNER                      133
+#define IDB_BALL1                       134
+#define IDB_BALL2                       135
+#define IDB_BALL3                       136
+#define IDB_BALL4                       137
+#define IDB_0                           138
+#define IDB_1                           139
+#define IDB_2                           140
+#define IDB_3                           141
+#define IDB_4                           142
+#define IDB_5                           143
+#define IDB_6                           144
+#define IDB_7                           145
+#define IDB_8                           146
+#define IDB_9                           147
+#define IDB_MINUS                       148
+#define IDB_CENTER                      149
+#define IDB_ERASER2                     150
+#define IDB_ERASER3                     151
+#define IDB_CONTINUE                    152
+#define IDC_GAMECURSOR                  153
+#define IDB_HELP                        155
+#define IDB_BITMAP2                     156
+#define IDB_LOADING                     156
+#define IDB_PNG1                        165
+#define IDB_PNG2                        166
+#define IDB_ELF_ORANGE_5                167
+#define IDB_ELF_BLUE_1                  168
+#define IDB_ELF_ORANGE_1                169
+#define IDB_ELF_PINK_1                  170
+#define IDB_ELF_RED_1                   171
+#define IDB_ELF_PINK_2                  173
+#define IDB_ELF_PINK_3                  174
+#define IDB_ELF_PINK_4                  175
+#define IDB_ELF_PINK_5                  176
+#define IDB_ELF_RED_2                   177
+#define IDB_ELF_RED_3                   178
+#define IDB_ELF_RED_4                   179
+#define IDB_ELF_RED_5                   180
+#define IDB_ELF_BLUE_2                  181
+#define IDB_ELF_BLUE_3                  182
+#define IDB_ELF_BLUE_4                  183
+#define IDB_ELF_BLUE_5                  184
+#define IDB_ELF_ORANGE_2                185
+#define IDB_ELF_ORANGE_3                186
+#define IDB_ELF_ORANGE_4                187
+#define IDB_GHOST_1                     188
+#define IDB_GHOST_2                     189
+#define IDB_GHOST_3                     190
+#define IDB_GHOST_4                     191
+#define IDB_BITMAP3                     192
+#define IDB_PACMAN_1                    192
+#define IDC_README                      1001
+#define ID_FILE_PAUSE                   32771
+#define ID_TOGGLE_FULLSCREEN            32772
+#define ID_BUTTON_FULLSCREEN            32773
+#define ID_BUTTON_PAUSE                 32774
+#define ID_BUTTON_UNITTEST              32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        193
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
